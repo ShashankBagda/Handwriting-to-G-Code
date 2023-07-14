63891ae705361a244af21fe156a82b11c8b1b3f5
Shashank Bagda <shashankbagda167@gmail.com>
2023-07-14 13:34:15 +0530

    step 1
    Square detection and cropping done

---
+++ crop.py
@@ -0,0 +1,68 @@
+import cv2
+import os
+
+def detect_and_crop_boxes(image_path, output_folder, square_size):
+    # Load the image
+    image = cv2.imread(image_path)
+    grayscale = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    # Apply edge detection to emphasize boundaries
+    edges = cv2.Canny(grayscale, 50, 150)
+
+    # Find contours in the edge image
+    contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    # Create the output folder if it doesn't exist
+    os.makedirs(output_folder, exist_ok=True)
+
+    # Crop and save each detected square box
+    for i, contour in enumerate(contours):
+        # Approximate the contour to a polygon
+        epsilon = 0.03 * cv2.arcLength(contour, True)
+        approx = cv2.approxPolyDP(contour, epsilon, True)
+
+        # Skip non-quadrilateral contours
+        if len(approx) != 4:
+            continue
+
+        # Sort the vertices in a clockwise order
+        box = cv2.boundingRect(approx.reshape(-1, 2))
+        (x, y, w, h) = box
+        if w < h:
+            (x, y, w, h) = (x, y, h, h)
+        else:
+            (x, y, w, h) = (x, y, w, w)
+
+        # Skip small or non-square boxes
+        aspect_ratio = w / float(h)
+        if w < 50 or h < 50 or aspect_ratio < 0.9 or aspect_ratio > 1.1:
+            continue
+
+        # Determine the size of the square region to crop
+        size = min(w, h)
+
+        # Calculate the cropping region
+        x_center = x + w // 2
+        y_center = y + h // 2
+        half_size = size // 2
+        x_min = x_center - half_size
+        y_min = y_center - half_size
+        x_max = x_center + half_size
+        y_max = y_center + half_size
+
+        # Crop the square region from the original image
+        cropped_box = image[y_min:y_max, x_min:x_max]
+
+        # Resize the cropped image to the desired square size
+        cropped_box = cv2.resize(cropped_box, (square_size, square_size))
+
+        # Save the cropped box as a separate image
+        output_path = os.path.join(output_folder, f'box_{i}.png')
+        cv2.imwrite(output_path, cropped_box)
+
+# Example usage
+image_path = 'image.png'
+output_folder = 'Images'
+square_size = 100  # Specify the desired square size in pixels
+
+detect_and_crop_boxes(image_path, output_folder, square_size)
