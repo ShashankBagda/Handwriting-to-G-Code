24b1a79e76412ce83e9a415a52fcc4c0c47f24ba
Shashank Bagda <shashankbagda167@gmail.com>
2024-02-15 14:25:27 +0530

    .

--- rename.py
+++ rename.py
@@ -8,7 +8,7 @@ k = 0
 
 num_arr = ['9', '8', '7', '6', '5', '4', '3', '2', '1', '0']
 alphabet_arr = ['z', 'y', 'x', 'w', 'v', 'Z', 'Y', 'X', 'W', 'V', 'u', 't', 's', 'r', 'q', 'U', 'T', 'S', 'R', 'Q', 'p', 'o', 'n', 'm', 'l', 'P', 'O', 'N', 'M', 'L', 'k', 'j', 'i', 'h', 'g', 'K', 'J', 'I', 'H', 'G', 'f', 'e', 'd', 'c', 'b', 'F', 'E', 'D', 'C', 'B', 'a', 'A']
-special_char = ['_semicolon','_double_InComma_start','_double_InComma_end','_underscore','_equals','_addition','_substraction','_brac_curl_end','_brac_curl_start','_brac_square_end','_brac_square_start','_multiplication','_hashtag','_and','_percentage','_dollar','_tilde','_brac_round_end','_brac_round_start','_greaterthan','_lessthan','_slash','_toPower','_atharet','_question','_exclamation','_colon','_comma','_apostrophe','_backquote','_backSlash']
+special_char = ['_semicolon','_full_stop','_double_InComma','_underscore','_equals','_addition','_substraction','_brac_curl_end','_brac_curl_start','_brac_square_end','_brac_square_start','_multiplication','_hashtag','_and','_percentage','_dollar','_tilde','_brac_round_end','_brac_round_start','_greaterthan','_lessthan','_slash','_toPower','_atharet','_question','_exclamation','_colon','_comma','_apostrophe','_backquote','_backSlash']
 
 file_list = os.listdir(folder_path)
 for i, filename in enumerate(file_list):
